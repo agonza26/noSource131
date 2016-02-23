902f930373aeeb309178e298f863491c9ebddf25
agonza26 <agonza26@ucsc.edu>
2016-02-23 08:21:42 -0800

    almost done with github

---
+++ noSource/Assets/SCRIPTS/CoreGameLoop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoreGameLoop : MonoBehaviour {
+
+
+	GameObject player;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		player.GetComponentInParent<Collider> ();
+	}
+}
