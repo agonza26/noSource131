2ce914ed1b68609ce945ef40f8825f769a63376e
kwchan28 <kwchan@ucsc.edu>
2016-02-29 16:13:36 -0800

    Main menu v2
    Files re-formated and menu changed to be canvas

---
+++ noSource/Assets/SCRIPTS/UIControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+
+    public void ChangeLevel(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void ChangeSettings(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void ReturnMain(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
