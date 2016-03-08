3dc880ee395c8b4827c700961028cb4458db9cf4
kwchan28 <kwchan@ucsc.edu>
2016-03-07 20:49:45 -0800

    Added locking
    Added locking functionality

---
+++ noSource/Assets/SCRIPTS/LockLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockLevel : MonoBehaviour
+{
+    public static int levels = 5; //number of levels
+
+    private int levelIndex;
+
+
+    void Start()
+    {
+        PlayerPrefs.DeleteAll(); //erase data on start
+        LockLevels();   //call function LockLevels
+    }
+
+    //function to lock the levels
+    void LockLevels()
+    {
+        //loop thorugh all the levels of all the worlds
+            for (int j = 1; j < levels; j++)
+            {
+                levelIndex = (j + 1);
+                //create a PlayerPrefs of that particular level and world and set it's to 0, if no key of that name exists
+                if (!PlayerPrefs.HasKey("level" + levelIndex.ToString()))
+                {
+                    PlayerPrefs.SetInt("level" + levelIndex.ToString(), 0);
+                }
+
+            }
+        }
+}
---
+++ noSource/Assets/SCRIPTS/LockScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LockScreen : MonoBehaviour
+{
+
+    private int levelIndex;
+
+    void Start()
+    {
+                CheckLockedLevels();
+    }
+
+    //Level to load on button click. Will be used for Level button click event 
+    public void Selectlevel(string level)
+    {
+        Application.LoadLevel("Level" + level); //load the level
+    }
+
+    //uncomment the below code if you have a main menu scene to navigate to it on clicking escape when in World1 scene
+    /*public void  Update (){
+     if (Input.GetKeyDown(KeyCode.Escape) ){
+      Application.LoadLevel("MainMenu");
+     }   
+    }*/
+
+    //function to check for the levels locked
+    void CheckLockedLevels()
+    {
+        //loop through the levels of a particular world
+        for (int j = 1; j < LockLevel.levels; j++)
+        {
+            levelIndex = (j + 1);
+            if ((PlayerPrefs.GetInt("level" + levelIndex.ToString())) == 1)
+            {
+                GameObject.Find("LockedLevel" + (levelIndex)).active = false;
+                Debug.Log("Unlocked");
+            }
+        }
+    }
+}
